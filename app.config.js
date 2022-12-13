46a6f6fcd6ca666047f3615131522170a316b1b3
Lena Eivy <applena@gmail.com>
2022-12-12 16:04:22 -0800

    adding a config

---
+++ app.config.js
@@ -0,0 +1,42 @@
+
+
+module.exports = {
+  "name": "homeschool-portfolio",
+  "slug": "homeschool-portfolio",
+  "version": "1.0.3",
+  "orientation": "portrait",
+  "icon": "./assets/icon.png",
+  "userInterfaceStyle": "light",
+  "splash": {
+    "image": "./assets/splash.png",
+    "resizeMode": "contain",
+    "backgroundColor": "#ffffff"
+  },
+  "updates": {
+    "fallbackToCacheTimeout": 0
+  },
+  "assetBundlePatterns": [
+    "**/*"
+  ],
+  "ios": {
+    "infoPlist": {
+      "NSPhotoLibraryUsageDescription": "Use of the camera allows users to take photos of the portfolio work"
+    },
+    "supportsTablet": true,
+    "bundleIdentifier": "com.homeschoolPortfolio.homeschoolportfolio"
+  },
+  "android": {
+    "adaptiveIcon": {
+      "foregroundImage": "./assets/adaptive-icon.png",
+      "backgroundColor": "#FFFFFF"
+    }
+  },
+  "web": {
+    "favicon": "./assets/favicon.png"
+  },
+  "extra": {
+    "eas": {
+      "projectId": "ccdba707-48fa-4a52-bc0d-162100609c81"
+    }
+  }
+}
